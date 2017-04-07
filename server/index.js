cb8dd3ceffe3720916d376802f70a20ece2d33b9
Vasiliy Troyan <trovas@inbox.ru>
2017-04-07 09:27:49 +0600

    Remove Redis using

--- server/index.js
+++ server/index.js
@@ -1,74 +1,50 @@
 let express = require('express');
 let async = require('async');
 let router = express.Router();
-let redisClient = require('redis'),
-    redisDB = redisClient.createClient();
 
 router.get('/api', function (req, res) {
     res.send('API is running');
 });
 
 router.get('/api/nt/:id', (req, res) => {
-  redisDB.hgetall(`nt${req.params.id}`, (err, nt_data) => {
-    if (!nt_data) {
-      console.error('Redis error. Not found nt:%s', req.params.id);
-      return res.status(404).send({error: `Not found nt:${req.params.id}`});
-    }
-    if (!err) {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).send(nt_data);
-    } else {
-      console.error("Redis error: " + err);
-      res.status(500).send({error: "Redis error: " + err})
-    }
-  });
+  let data = require('./udp_listener').data;
+  if (`nt${req.params.id}` in data)
+    res.status(200).send(data[`nt${req.params.id}`]);
+  else
+    res.status(404).send({error: 'table not found'});
 });
 
 router.get('/api/micdata', (req, res) => {
   let paramList = req.query.paramList;
-  let mdata = {};
-  let ntnsRe = /nt(\d+)ns(\d+)/;
-   
-  async.each(
-    paramList,
-    function(prm, callback) {
-      let [nt_id, ns_id] = ntnsRe.exec(prm).slice(1, 3);
-      redisDB.hmget(`nt${nt_id}`, `ns${ns_id}`, (err, ns_value) => {
-        if (err) {
-          console.error(err);
-          res.status(200).end("Server error");
-          return;
-        }
-        if (!(`nt${nt_id}` in mdata)) mdata[`nt${nt_id}`] = {};
-
-        mdata[`nt${nt_id}`][`ns${ns_id}`] = ns_value[0];
-        callback();
-      });
-    },
-    function(err) {
-      if (err) {
-        console.error(err);
-        res.status(200).end("Server error");
-        return;
-      }
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).send(JSON.stringify(mdata));
-    }
-  );
+  let data = require('./udp_listener').data;
+  let res_data = {};
+  let ntnsRe = /(nt\d+)(ns\d+)/;
+  paramList.map((tag) => {
+    let [nt_id, ns_id] = tag.match(ntnsRe).slice(1, 3);
+    if (!(nt_id in res_data)) res_data[nt_id] = {};
+    // При перезапуске не всегда успевают придти данные по всем таблицам
+    // приходится подставлять ноль
+    res_data[nt_id][ns_id] = data[nt_id]?(data[nt_id][ns_id]?data[nt_id][ns_id]:0):0;
+  });
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(JSON.stringify(res_data));
 });
 
 router.get('/api/nt/:nt_id/ns/:ns_id', (req, res) => {
-  redisDB.hmget(`nt${req.params.nt_id}`, `ns${req.params.ns_id}`, (err, ns_value) => {
-    if (!ns_value) {
-      return res.status(404).send({error: `Not found param nt:${req.params.nt_id} ns:${req.params.ns_id}`});
-    }
-    if (!err) {
-      res.send(ns_value);
-    } else {
-      console.error("Redis error: " + err);
-      res.status(500).send({error: "Redis error: " + err})
-    }
-  });
+  let data = require('./udp_listener').data;
+  let nt = req.params.nt_id;
+  let ns = req.params.ns_id;
+  if ((`nt${nt}` in data) && (`ns${ns}` in data[`nt${nt}`])) {
+    res.setHeader('Content-Type', 'application/json');
+    let res_data = {};
+    res_data[`nt${nt}`] = {};
+    res_data[`nt${nt}`].nt = data[`nt${nt}`].nt;
+    res_data[`nt${nt}`].utime = data[`nt${nt}`].utime;
+    res_data[`nt${nt}`][`ns${ns}`] = data[`nt${nt}`][`ns${ns}`];
+    res.status(200).send(JSON.stringify(res_data));
+  } else {
+    res.status(404).send({error: 'param not found'});
+  }
 });
 
 router.get('/api/tagcache', (req, res) => {
